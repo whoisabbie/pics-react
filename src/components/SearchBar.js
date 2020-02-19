66115b1b0567995ecd5ecfdab0d6ebe72ba52155
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-02-19 18:49:33 +0530

    Final commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+    state = {
+        term: ''
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            autoFocus
+                            value={this.state.term} type="text"
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
